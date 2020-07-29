2b1ab2cb4c6c257bce987ffeb61ced1d6631c6ae
Abhishek Shinde <49138785+shindeabhi27@users.noreply.github.com>
2020-07-29 18:49:04 -0400

    Add files via upload

---
+++ Pages/Games/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Games
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly WebApplication1.Data.AppDbContext _context;
+
+        public DeleteModel(WebApplication1.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Game Games { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Games = await _context.Games.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Games == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Games = await _context.Games.FindAsync(id);
+
+            if (Games != null)
+            {
+                _context.Games.Remove(Games);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Games/Edit.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Games
+{
+    public class EditModel : PageModel
+    {
+        private readonly WebApplication1.Data.AppDbContext _context;
+
+        public EditModel(WebApplication1.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Game Games { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Games = await _context.Games.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Games == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Games).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GamesExists(Games.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool GamesExists(int id)
+        {
+            return _context.Games.Any(e => e.Id == id);
+        }
+    }
+}
